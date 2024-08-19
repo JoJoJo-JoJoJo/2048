5d85c4be225f1e6a0326ce4b49c57764bba46eda
JoJo <jonahthwaite@gmail.com>
2024-08-19 20:16:57 +0100

    Update HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,15 @@
+export default class HomeScreen {
+  #containerDiv;
+
+  constructor(_gameBoard) {
+    this.#containerDiv = _gameBoard;
+  }
+
+  showHomeScreen() {
+    // This will just make the home screen appear and function.
+  }
+
+  hideHomeScreen() {
+    // This will stop the home screen from functioning, and make it disappear.
+  }
+}
